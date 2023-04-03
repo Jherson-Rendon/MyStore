b7b4a0a133806dde7d24be0e866c1cf9cf7c4394
Jonathan Arango <jonathan.arango@utp.edu.co>
2023-04-02 19:04:41 -0500

    solucinado

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const productDetailCloseIcon = document.querySelector('product-detail-close');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
-const productDetailContainer= document.querySelector('#productDetail');
-
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -15,60 +14,65 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function toggleDesktopMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    const isAsideClosed = shoppingCartConainer.classList.contains('inactive');
-    if (!isAsideClosed) {
-        shoppingCartContainer.classList.add('inactive');
-    }
-    desktopMenu.classList.toggle('inactive');
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = shoppingCartConainer.classList.contains('inactive');
-    if (!isAsideClosed) {
-        shoppingCartContainer.classList.add('inactive');
-    }
-    closeProductDetailAside();
-    mobileMenu.classList.toggle('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive'); 
+  }
+
+  closeProductDetailAside();
+  
+  mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside() {
-    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
   
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
 
-    if (!isMobileMenuClosed) {
-        mobileMenu.classList.add('inactive');
-    }
-
-    const isProductDetailClosed = productDetailContainer.contains('inactive');
-    if (!isProductDetailClosed) {
-        productDetailContainer.classList.contains('inactive');
-    }
-    shoppingCartContainer.classList.toggle('inactive');
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  }
+  
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
-function openProductDetailAside(){
-    shoppingCartContainer.classList.add('inactive');
-    productDetailContainer.classList.remove('inactive');
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
 }
 
-function closeProductDetailAside(){
-    productDetailContainer.classList.add('inactive');
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
 
 productList.push({
     name: 'Bike',
-    price:120,
-    image:'https://img.freepik.com/vector-gratis/fondo-bicicleta-roja-campo_23-2147557902.jpg?w=740&t=st=1679852320~exp=1679852920~hmac=c2da99712470d6a880280ae36278c36a7cea5cac714467482b661ce90e7d3f7b'
+    price: 120,
+    image: 'https://img.freepik.com/vector-gratis/fondo-bicicleta-roja-campo_23-2147557902.jpg?w=740&t=st=1679852320~exp=1679852920~hmac=c2da99712470d6a880280ae36278c36a7cea5cac714467482b661ce90e7d3f7b'
 });
 
 
 productList.push({
     name: 'Coca cola',
-    price:45,
-    image:'https://www.eleconomista.com.mx/__export/1637290902063/sites/eleconomista/img/2021/11/18/coca_shutterstock_214810693.jpg_1902800913.jpg'
+    price: 45,
+    image: 'https://www.eleconomista.com.mx/__export/1637290902063/sites/eleconomista/img/2021/11/18/coca_shutterstock_214810693.jpg_1902800913.jpg'
 });
 
 
@@ -84,48 +88,47 @@ productList.push({
                     </figure>
                 </div>
             </div>*/
-function renderProducts(arr)
-{
-    
-for (product of arr){
-    const productCard =document.createElement('div');
-    productCard.classList.add('product-card');
+function renderProducts(arr) {
 
-    const productImg = document.createElement('img');
-    productImg.setAttribute('src',product.image);
-    productImg.addEventListener('click',openProductDetailAside);
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
 
-    const productInfo =document.createElement('div');
-    productInfo.classList.add('product-info');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
 
-    const productInfoDiv =document.createElement('div');
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
 
-    const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price;
+        const productInfoDiv = document.createElement('div');
 
-    const productName = document.createElement('p');
-    productName.innerText = product.name;
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
 
-    productInfoDiv.appendChild(productPrice);
-    productInfoDiv.appendChild(productName);
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
 
 
-    const productInfoFigure = document.createElement('figure');
+        const productInfoFigure = document.createElement('figure');
 
-    const productImgCart = document.createElement('img');
-    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
 
-    productInfoFigure.appendChild(productImgCart);
+        productInfoFigure.appendChild(productImgCart);
 
-productInfo.appendChild(productInfoDiv);
-productInfo.appendChild(productInfoFigure);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
 
-productCard.appendChild(productImg);
-productCard.appendChild(productInfo);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
 
-cardsContainer.appendChild(productCard);
+        cardsContainer.appendChild(productCard);
 
-}
+    }
 }
 
 renderProducts(productList);
